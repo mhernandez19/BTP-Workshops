1509f247fcaf2dd0cddfe5fe44f4d585e8eb5c3d
mhernandez19 <43587013+mhernandez19@users.noreply.github.com>
2018-09-25 16:01:30 -0400

    Update temps.c

--- WS03/in_lab/temps.c
+++ WS03/in_lab/temps.c
@@ -1,9 +1,19 @@
-// Name:
-// Student Number:
-// Email:
-// Section:
-// Workshop:
+// Name: Maria Hernandez
+// Student Number:128627189
+// Email: mhernandez19@myseneca.ca
+// Section:in-lab
+// Workshop: workshop 3
 
 #define _CRT_SECURE_NO_WARNINGS
 
 // Put your code below:
+#define NUMS 3
+---===IPC Temperature Analyzer===---
+  Enter the high value for day 1: 
+
+    
+    Enter the low value for day 1:
+
+    Incorrect values, temperatures must be in the range -40 to 40, high must be greater than low.
+      
+      The average (mean) temperature was:--
